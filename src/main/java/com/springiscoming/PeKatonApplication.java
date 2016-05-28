27d4867fafa01211dd704c40e24a4fe26bfa6f10
Mateusz Brycki <mateusz.brycki@outlook.com>
2016-05-28 13:21:23 +0200

    master - initial commit

---
+++ src/main/java/com/springiscoming/PeKatonApplication.java
@@ -0,0 +1,12 @@
+package com.springiscoming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeKatonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeKatonApplication.class, args);
+	}
+}
---
+++ src/main/java/com/springiscoming/model/Student.java
@@ -0,0 +1,55 @@
+package com.springiscoming.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "STUDENT")
+public class Student {
+
+    @Id
+    @GeneratedValue
+    private String id;
+    private String firstname;
+    private String secondname;
+
+    public Student() {
+    }
+
+    public Student(String firstname, String secondname) {
+        this.firstname = firstname;
+        this.secondname = secondname;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getSecondname() {
+        return secondname;
+    }
+
+    public void setSecondname(String secondname) {
+        this.secondname = secondname;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id='" + id + '\'' +
+                ", firstname='" + firstname + '\'' +
+                ", secondname='" + secondname + '\'' +
+                '}';
+    }
+}
