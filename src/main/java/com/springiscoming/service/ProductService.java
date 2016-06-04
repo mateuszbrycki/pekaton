0858280ed24a84bb32b875cd358fccf74afd65d3
winio94 <winio94@gmail.com>
2016-06-04 14:47:24 +0200

    default order model

--- src/main/java/com/springiscoming/service/ProductService.java
+++ src/main/java/com/springiscoming/service/ProductService.java
@@ -5,6 +5,7 @@ import com.springiscoming.repository.ProductRepository;
 import org.springframework.stereotype.Service;
 
 import javax.inject.Inject;
+import java.util.List;
 
 /**
  * Created by winio_000 on 2016-06-04.
@@ -17,5 +18,9 @@ public class ProductService {
 
     public void save(Product product) {
         productRepository.save(product);
+    }
+
+    public List<Product> getAll() {
+        return productRepository.findAll();
     }
 }
---
+++ src/main/java/com/springiscoming/service/PurchaseService.java
@@ -0,0 +1,22 @@
+package com.springiscoming.service;
+
+import com.springiscoming.model.Purchase;
+import com.springiscoming.repository.PurchaseRepository;
+import org.springframework.stereotype.Service;
+
+import javax.inject.Inject;
+
+/**
+ * Created by winio_000 on 2016-06-04.
+ */
+
+@Service
+public class PurchaseService {
+
+    @Inject
+    private PurchaseRepository purchaseRepository;
+
+    public Purchase saveOrder(Purchase purchase) {
+        return purchaseRepository.save(purchase);
+    }
+}
