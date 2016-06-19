7b720799122f7fa36ed6117d536dadd555aa8ec5
winio94 <winio94@gmail.com>
2016-06-19 23:16:42 +0200

    refactor promising customer service

--- src/main/java/com/springiscoming/service/promising/PromisingCustomersService.java
+++ src/main/java/com/springiscoming/service/promising/PromisingCustomersService.java
@@ -3,12 +3,9 @@ package com.springiscoming.service.promising;
 import com.springiscoming.model.Customer;
 import com.springiscoming.model.promising.PromisingCustomer;
 import com.springiscoming.service.CustomerService;
-import com.springiscoming.service.PurchaseService;
-import com.springiscoming.service.SiteEntryService;
 import org.springframework.stereotype.Service;
 
 import javax.inject.Inject;
-import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -22,69 +19,26 @@ public class PromisingCustomersService {
     private CustomerService customerService;
 
     @Inject
-    private SiteEntryService siteEntryService;
+    private PromisingCustomerProvider promisingCustomerProvider;
 
-    @Inject
-    private PurchaseService purchaseService;
+    public List<PromisingCustomer> findPromisingCustomers() {
+        return promisingCustomers(customerService.findAll());
+    }
 
-    public List<PromisingCustomer> findPromisingCustomersList() {
-
+    private List<PromisingCustomer> promisingCustomers(List<Customer> customers) {
         List<PromisingCustomer> result = new ArrayList<>();
-        List<Customer> customersList = customerService.findAll();
+        for (Customer customer : customers) {
+            PromisingCustomer promisingCustomer = promisingCustomerProvider.get(customer);
 
-        for(Customer customer : customersList) {
-
-            PromisingCustomer promisingCustomer = new PromisingCustomer();
-            promisingCustomer.setCustomer(customer);
-
-            Float promisingCustomerFactor = calculatePromisingFactor(promisingCustomer);
-
-            if(promisingCustomerFactor != null && promisingCustomerFactor > 0) {
-                promisingCustomer.setFactor(promisingCustomerFactor);
+            if (isPositive(promisingCustomer.getFactor())) {
                 result.add(promisingCustomer);
             }
         }
 
         return result;
-
     }
 
-    private Float calculatePromisingFactor(PromisingCustomer promisingCustomer) {
-
-        Long customerId = promisingCustomer.getCustomer().getCustomerId();
-
-        //prepare factor data
-        Float averageVisitTime = siteEntryService.getCustomerAverageVisitTime(customerId);
-        Integer visitsCount = siteEntryService.getCustomerVisitsCounter(customerId);
-        Integer directVisitsCount = siteEntryService.getCustomerDirectVisitsCounter(customerId);
-
-        Integer purchasesCount = purchaseService.getPurchasesCounter(customerId);
-        Float purchasesSummaryCost = purchaseService.getPurchasesSummaryCost(customerId);
-
-        promisingCustomer.setAverageVisitTime(averageVisitTime);
-        promisingCustomer.setVisitsCount(visitsCount);
-        promisingCustomer.setDirectVisitsCount(directVisitsCount);
-
-        promisingCustomer.setPurchasesCount(purchasesCount);
-        promisingCustomer.setPurchasesSummaryCost(purchasesSummaryCost);
-
-        /*System.out.println("CustomerId: " + customerId +
-                ", averageVisitTime: " + averageVisitTime +
-                ", purchasesCount: " + purchasesCount +
-                ", directVisitsCount: " + directVisitsCount +
-                ", purchasesSummaryCost: " + purchasesSummaryCost +
-                ", visitsCount: " + visitsCount);*/
-
-
-        Float visitCountFactor = new BigDecimal(Math.pow(visitsCount.doubleValue(), 2)).floatValue() * 0.1f;
-        Float directVisitsFactor = directVisitsCount* 0.1f;
-        Float averageVisitsFactor = averageVisitTime * 0.2f;
-
-        Float purchasesCountFactor = purchasesCount * 0.4f;
-        Float purchasesProportionFactor = (purchasesSummaryCost / purchasesCount) * 0.5f;
-
-        Float factor = (purchasesCountFactor * directVisitsFactor * purchasesProportionFactor * averageVisitsFactor) / (visitCountFactor * 100);
-
-        return factor;
+    private boolean isPositive(Float promisingCustomerFactor) {
+        return promisingCustomerFactor != null && promisingCustomerFactor > 0;
     }
 }
---
+++ src/test/java/com/springiscoming/service/promising/PromisingCustomersServiceTest.java
@@ -0,0 +1,114 @@
+package com.springiscoming.service.promising;
+
+import com.springiscoming.model.Customer;
+import com.springiscoming.model.promising.PromisingCustomer;
+import com.springiscoming.service.CustomerService;
+import com.springiscoming.service.builders.CustomerBuilder;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.util.Arrays.asList;
+import static java.util.Collections.emptyList;
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+/**
+ * Created by winio_000 on 2016-06-19.
+ */
+
+@RunWith(MockitoJUnitRunner.class)
+public class PromisingCustomersServiceTest {
+
+    public static final float ZERO = 0f;
+    public static final float ONE = 1f;
+
+    @Mock
+    private CustomerService customerService;
+
+    @Mock
+    private PromisingCustomerProvider promisingCustomerProvider;
+
+    @InjectMocks
+    private PromisingCustomersService promisingCustomersService;
+
+    @Test
+    public void shouldReturnEmptyListWhenNoneCustomerGiven() throws Exception {
+        when(customerService.findAll()).thenReturn(emptyList());
+
+        List<PromisingCustomer> promisingCustomers = promisingCustomersService.findPromisingCustomers();
+
+        assertTrue(promisingCustomers.isEmpty());
+    }
+
+    @Test
+    public void shouldReturnEmptyListWhenNoneOfTheCustomersHasPositiveFactor() throws Exception {
+        Customer customer1 = customer(1L);
+        Customer customer2 = customer(2L);
+        PromisingCustomer promisingCustomer1 = mock(PromisingCustomer.class);
+        PromisingCustomer promisingCustomer2 = mock(PromisingCustomer.class);
+        when(promisingCustomer1.getFactor()).thenReturn(ZERO);
+        when(promisingCustomer2.getFactor()).thenReturn(ZERO);
+        when(customerService.findAll()).thenReturn(customers(customer1, customer2));
+        when(promisingCustomerProvider.get(customer1)).thenReturn(promisingCustomer1);
+        when(promisingCustomerProvider.get(customer2)).thenReturn(promisingCustomer2);
+
+        List<PromisingCustomer> promisingCustomers = promisingCustomersService.findPromisingCustomers();
+
+        assertTrue(promisingCustomers.isEmpty());
+    }
+
+    @Test
+    public void shouldReturnOneTheOnlyOnePromisingCustomerWithPositiveFactor() throws Exception {
+        Customer customer1 = customer(1L);
+        Customer customer2 = customer(2L);
+        PromisingCustomer promisingCustomer1 = mock(PromisingCustomer.class);
+        PromisingCustomer promisingCustomer2 = mock(PromisingCustomer.class);
+        when(promisingCustomer1.getFactor()).thenReturn(ZERO);
+        when(promisingCustomer2.getFactor()).thenReturn(ONE);
+        when(customerService.findAll()).thenReturn(customers(customer1, customer2));
+        when(promisingCustomerProvider.get(customer1)).thenReturn(promisingCustomer1);
+        when(promisingCustomerProvider.get(customer2)).thenReturn(promisingCustomer2);
+
+        List<PromisingCustomer> promisingCustomers = promisingCustomersService.findPromisingCustomers();
+
+        assertEquals(promisingCustomers.size(), 1);
+        assertTrue(promisingCustomers.contains(promisingCustomer2));
+        assertFalse(promisingCustomers.contains(promisingCustomer1));
+    }
+
+    @Test
+    public void shouldPromisingCustomersForAllCustomersWhenAllHavePositiveFactor() throws Exception {
+        Customer customer1 = customer(1L);
+        Customer customer2 = customer(2L);
+        PromisingCustomer promisingCustomer1 = mock(PromisingCustomer.class);
+        PromisingCustomer promisingCustomer2 = mock(PromisingCustomer.class);
+        when(promisingCustomer1.getFactor()).thenReturn(ONE);
+        when(promisingCustomer2.getFactor()).thenReturn(ONE);
+        when(customerService.findAll()).thenReturn(customers(customer1, customer2));
+        when(promisingCustomerProvider.get(customer1)).thenReturn(promisingCustomer1);
+        when(promisingCustomerProvider.get(customer2)).thenReturn(promisingCustomer2);
+
+        List<PromisingCustomer> promisingCustomers = promisingCustomersService.findPromisingCustomers();
+
+        assertEquals(promisingCustomers.size(), 2);
+        assertTrue(promisingCustomers.contains(promisingCustomer2));
+        assertTrue(promisingCustomers.contains(promisingCustomer1));
+    }
+
+    private Customer customer(Long customerId) {
+        return CustomerBuilder.get()
+                .withCustomerId(customerId)
+                .build();
+    }
+
+    private List<Customer> customers(Customer... customers) {
+        return new ArrayList<>(asList(customers));
+    }
+}
